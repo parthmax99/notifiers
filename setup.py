282e6b7a11b1e78c14a1fd26df081cddf70b5957
liiight <ocarmi@proofpoint.com>
2018-04-30 01:23:37 +0300

    added py3.7 classifier

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop"
     ],
